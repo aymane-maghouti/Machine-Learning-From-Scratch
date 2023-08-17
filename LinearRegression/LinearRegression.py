d13fc66b86b5a76bd95eade8005d5d900e4f0099
AymaneMG <aymanemaghouti16@gmail.com>
2023-08-17 11:53:58 +0100

    Linear Regression

---
+++ LinearRegression/LinearRegression.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def mean_sqaure_error(y_test, predictions):
+    return np.mean((y_test-predictions)**2)
+
+class LinearRegression:
+
+    def __init__(self, lr = 0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            y_pred = np.dot(X, self.weights) + self.bias
+
+            dw = (1/n_samples) * np.dot(X.T, (y_pred-y))
+            db = (1/n_samples) * np.sum(y_pred-y)
+
+            self.weights = self.weights - self.lr * dw
+            self.bias = self.bias - self.lr * db
+
+    def predict(self, X):
+        y_pred = np.dot(X, self.weights) + self.bias
+        return y_pred
